d4165633bea5099104bae1387723c8d76d94e722
iva1111 <mvg75@mail.ru>
2021-08-18 20:51:23 +0500

    initial commit

---
+++ src/main/java/ru/netology/domain/PostFilms.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PostFilms {
+    public int length;
+    private int id;
+    private int filmId;
+    private String filmName;
+    // private int count;
+}
+
---
+++ src/test/java/ru/netology/manager/PostFilmsTest.java
@@ -0,0 +1,13 @@
+package ru.netology.manager;
+
+import ru.netology.domain.PostFilms;
+import ru.netology.manager.Manager;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PostFilmsTest {
+    @Test
+    public void test1() {
+
+    }
+}
